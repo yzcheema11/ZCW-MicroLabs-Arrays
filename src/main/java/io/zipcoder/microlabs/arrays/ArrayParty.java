2a15d8a4b5d1fb43d595335b844752a067a98c1e
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2017-05-09 11:33:07 -0400

    First check in

---
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -0,0 +1,26 @@
+package io.zipcoder.microlabs.arrays;
+
+
+public class ArrayParty {
+
+    public String printArray(String[] inputArray){
+        return "";
+    }
+
+    public String lastElement(String[] inputArray){
+        return "";
+    }
+
+    //TODO Define the method lastButOne
+
+    //TODO Define the method reverse
+
+    //TODO Define the method isPalindrome
+
+    //TODO Define the method compress
+
+    //TODO Define the method pack
+
+
+
+}
---
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -0,0 +1,48 @@
+package io.zipcoder.microlabs.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayPartyTest {
+
+    @Test
+    public void printArrayTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Sausage\n" +
+                "Eggs\n" +
+                "Beans\n" +
+                "Bacon\n" +
+                "Tomatoes\n" +
+                "Mushrooms";
+
+        //: When
+        String actual = arrayParty.printArray(breakfast);
+
+        //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
+    }
+
+    @Test
+    public void lastElementTest(){
+        //: Given
+
+        //: When
+
+        //: Then
+    }
+
+    //TODO Define the method lastButOneTest
+
+    //TODO Define the method reverseTest
+
+    //TODO Define the method isPalindromeTest
+
+    //TODO Define the method compressTest
+
+    //TODO Define the method packTest
+
+
+}
